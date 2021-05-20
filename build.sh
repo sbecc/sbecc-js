47d1152d8dc3def01b241abe1a72a99e69fa3cc2
romdotdog <romdotdog@users.noreply.github.com>
2021-05-20 11:59:29 -0500

    Organize distribution

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 cd sbecc-c
-emcc -Isecp256k1/src -Ilibsodium/src/libsodium/include -Llibsodium/src/libsodium/.libs -lsodium -o ../sbecc.js sbecc.c -s MODULARIZE -s EXPORT_NAME=sbecc -s WASM=1 -s EXPORTED_RUNTIME_METHODS=stackAlloc,stringToUTF8,lengthBytesUTF8 -s EXPORTED_FUNCTIONS='["_sbecc_create_context","_sbecc_encrypt","_sbecc_destroy_context"]' -O3
+emcc -Isecp256k1/src -Ilibsodium/src/libsodium/include -Llibsodium/src/libsodium/.libs -lsodium -o ../dist/sbecc.js sbecc.c -s MODULARIZE -s EXPORT_NAME=sbecc -s WASM=1 -s EXPORTED_RUNTIME_METHODS=stackAlloc,stringToUTF8,lengthBytesUTF8 -s EXPORTED_FUNCTIONS='["_sbecc_create_context","_sbecc_encrypt","_sbecc_destroy_context"]' -O3
 cd ..
