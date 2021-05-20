dd0aafb94303c62f67eefad90df90a09dc443520
romdotdog <romdotdog@users.noreply.github.com>
2021-05-20 12:00:13 -0500

    Adjust tests on account of organization

--- tests/node.js
+++ tests/node.js
@@ -1,4 +1,4 @@
-require("../").then(m => {
+require("../dist").then(m => {
 	m.createContext()
 	console.log(m.encrypt("print('Hello, world!')").reduce(
 		(str, i) => str + ("0" + i.toString(16)).slice(-2),
